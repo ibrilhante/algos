ec3f74676d11804e859ab834a02ead4d392fcac9
ibrilhante <80493360+ibrilhante@users.noreply.github.com>
2021-08-26 23:44:04 -0700

    Update Reverse Number.js

--- Reverse Number.js
+++ Reverse Number.js
@@ -5,6 +5,10 @@
 
 /**Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.*/
 
- var reverse = function(x) {
-    return(parseFloat(x.toString().split('').reverse().join(''))*Math.sign(x))
+var reverse = function(x) {
+    if (x > 2147483647 || x < -2147483647) {
+        return(0);
+    } else{
+        return(parseFloat(x.toString().split('').reverse().join(''))*Math.sign(x));
+        }
 };
